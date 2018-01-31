746a191d70487aeda7f7da2e6593326eeb60a5e6
Sergey <bayduzh89@gmail.com>
2018-01-31 18:23:31 +0200

    31.01.18 17^30

--- resources/views/admin/objects/create.blade.php
+++ resources/views/admin/objects/create.blade.php
@@ -7,8 +7,8 @@
         <div class=" col-12">
             {!! Form::open(['method'=>'POST', 'action'=>['AdminObjectController@store']]) !!}
             <div class="form-group row">
-                {!! Form::label('lgFormGroupInput','Тип операции', ['class' => 'col-sm-4 col-form-label col-form-label-lg']) !!}
-                <div class="col-sm-6 position-relative">
+                {!! Form::label('lgFormGroupInput','Тип операции', ['class' => 'col-sm-3 col-form-label col-form-label-lg']) !!}
+                <div class="col-sm-7 position-relative">
                     {!! Form::select('operation',array(""=>'Выберите операцию') + $objectOperation ,null,['class'=>'form-control form-control-lg d-inline-block']) !!}
                     <button class="btn btn-info position-absolute custom_field_add_btn" value="operation" type="button">
                         <i
@@ -16,74 +16,63 @@
                 </div>
             </div>
             <div class="form-group row">
-                {!! Form::label('lgFormGroupInput','Тип объекта', ['class' => 'col-sm-4 col-form-label col-form-label-lg']) !!}
-                <div class="col-sm-6 position-relative">
+                {!! Form::label('lgFormGroupInput','Тип объекта', ['class' => 'col-sm-3 col-form-label col-form-label-lg']) !!}
+                <div class="col-sm-7 position-relative">
                     {!! Form::select('type',array(""=>'Выберите тип объекта') + $objectType ,null,['class'=>'form-control form-control-lg d-inline-block']) !!}
                     <button class="btn btn-info position-absolute custom_field_add_btn" value="type" type="button"><i
                                 class="fa fa-plus"></i></button>
                 </div>
             </div>
             <div class="form-group row">
-                {!! Form::label('lgFormGroupInput','Месторасположение объекта', ['class' => 'col-sm-4 col-form-label col-form-label-lg']) !!}
-                <div class="col-sm-6 position-relative">
+                {!! Form::label('lgFormGroupInput','Месторасположение объекта', ['class' => 'col-sm-3 col-form-label col-form-label-lg']) !!}
+                <div class="col-sm-7 position-relative">
                     {!! Form::select('place',array(""=>'Месторасположение') + $objectPlace ,null,['class'=>'form-control form-control-lg d-inline-block']) !!}
                     <button class="btn btn-info position-absolute custom_field_add_btn" value="place" type="button"><i
                                 class="fa fa-plus"></i></button>
                 </div>
             </div>
+
+
             <div class="form-group row">
-                {!! Form::label('lgFormGroupInput','Заголовок', ['class' => 'col-sm-4 col-form-label col-form-label-lg']) !!}
-                <div class="col-sm-6 position-relative">
+                <div class="form-group offset-3 col-4  d-flex align-items-center">
+                    {!! Form::label('lgFormGroupInput','Кол-во комнат', ['class' => 'col-sm-4 col-form-label ']) !!}
+                    {!! Form::text('title',null,['class'=>'form-control w-25   d-inline-block']) !!}
+                </div>
+                <div class="form-group col-4  d-flex align-items-center">
+                    {!! Form::label('lgFormGroupInput','Площадь', ['class' => 'col-sm-4 col-form-label ']) !!}
+                    {!! Form::text('title',null,['class'=>'form-control w-25   d-inline-block']) !!}
+
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="form-group offset-3 col-4  d-flex align-items-center">
+                    {!! Form::label('lgFormGroupInput','Опубликовать', ['class' => 'col-sm-4 col-form-label ']) !!}
+                    {{Form::checkbox('is_active', 'value',null,['class'=>''], true)}}
+                </div>
+                <div class="form-group col-4  d-flex align-items-center">
+                    {!! Form::label('lgFormGroupInput','Срочное', ['class' => 'col-sm-4 col-form-label ']) !!}
+                    {{Form::checkbox('is_urgent', 'value', false)}}
+                </div>
+            </div>
+
+
+            <div class="form-group row">
+                {!! Form::label('lgFormGroupInput','Заголовок', ['class' => 'col-sm-3 col-form-label col-form-label-lg']) !!}
+                <div class="col-sm-8 position-relative">
                     {!! Form::text('title',null,['class'=>'form-control form-control-lg d-inline-block']) !!}
                 </div>
             </div>
             <div class="form-group row">
-                {!! Form::label('lgFormGroupInput','Текс объяления', ['class' => 'col-sm-4 col-form-label col-form-label-lg']) !!}
-                <div class="col-sm-6 position-relative">
+                {!! Form::label('lgFormGroupInput','Текс объяления', ['class' => 'col-sm-3 col-form-label col-form-label-lg']) !!}
+                <div class="col-sm-8 position-relative">
                     {!! Form::textarea('title',null,['class'=>'', 'id' => 'ckview']) !!}
                 </div>
             </div>
+            <div class="col-12 clearfix">
+                {!! Form::submit('Добавить', ['class'=>'btn btn-success pull-right']) !!}
+            </div>
             {!! Form::close() !!}
         </div>
-        {{--<div class="col-6 ">--}}
-        {{--<div class="d-none add_operation">{!! Form::open(['method'=>'POST', 'action'=>['AdminOperationController@store']]) !!}--}}
-        {{--<div class="form-group row">--}}
-        {{--{!! Form::label('lgFormGroupInput','Добавить операцию', ['class' => 'col-sm-4 col-form-label col-form-label-lg']) !!}--}}
-        {{--<div class="col-sm-4">--}}
-        {{--{!! Form::text('operation',null,['class'=>'form-control form-control-lg d-inline-block']) !!}--}}
-        {{--</div>--}}
-        {{--<div class="col-sm-4">--}}
-        {{--{!! Form::submit('Добавить', ['class'=>'btn btn-success ']) !!}--}}
-        {{--</div>--}}
-        {{--</div>--}}
-        {{--{!! Form::close() !!}--}}
-        {{--</div>--}}
-
-        {{--<div class="d-none add_type">{!! Form::open(['method'=>'POST', 'action'=>['AdminTypeController@store']]) !!}--}}
-        {{--<div class="form-group row">--}}
-        {{--{!! Form::label('lgFormGroupInput','Добавить тип объекта', ['class' => 'col-sm-4 col-form-label col-form-label-lg']) !!}--}}
-        {{--<div class="col-sm-4">--}}
-        {{--{!! Form::text('type',null,['class'=>'form-control form-control-lg d-inline-block']) !!}--}}
-        {{--</div>--}}
-        {{--<div class="col-sm-4">--}}
-        {{--{!! Form::submit('Добавить', ['class'=>'btn btn-success ']) !!}--}}
-        {{--</div>--}}
-        {{--</div>--}}
-        {{--{!! Form::close() !!}--}}
-        {{--</div>--}}
-        {{--<div class="d-none add_place">{!! Form::open(['method'=>'POST', 'action'=>['AdminPlaceController@store']]) !!}--}}
-        {{--<div class="form-group row">--}}
-        {{--{!! Form::label('lgFormGroupInput','Добавить месторасположение', ['class' => 'col-sm-4 col-form-label col-form-label-lg']) !!}--}}
-        {{--<div class="col-sm-4">--}}
-        {{--{!! Form::text('place',null,['class'=>'form-control form-control-lg d-inline-block']) !!}--}}
-        {{--</div>--}}
-        {{--<div class="col-sm-4">--}}
-        {{--{!! Form::submit('Добавить', ['class'=>'btn btn-success ']) !!}--}}
-        {{--</div>--}}
-        {{--</div>--}}
-        {{--{!! Form::close() !!}--}}
-        {{--</div>--}}
-        {{--</div>--}}
 
         <div class="modal fade attr_modul" tabindex="-1" role="dialog" id="modalOperation" aria-labelledby="modalLabel"
              aria-hidden="true">
@@ -181,5 +170,5 @@
 @section('extrajavascript')
     <script src="{{url('tinymce/js/tinymce/jquery.tinymce.min.js')}}"></script>
     <script src="{{url('tinymce/js/tinymce/tinymce.min.js')}}"></script>
-    <script>tinymce.init({ selector:'#ckview' });</script>
+    <script>tinymce.init({selector: '#ckview'});</script>
 @endsection
