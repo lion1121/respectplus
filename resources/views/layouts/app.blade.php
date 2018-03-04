d5e531c25c95715e29365f9b1d01a67d14d63955
lion1121 <bayduzh89@gmail.com>
2018-03-04 20:06:17 +0200

    added active link function to contact link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -67,7 +67,7 @@
                         </div>
                     </li>
                     <li class="menu_item"><a href="{{route('news')}}" class="menu_link {{active_menu(\Illuminate\Support\Facades\Route::currentRouteName(),'news',0,4 )}}">новости</a></li>
-                    <li class="menu_item"><a href="{{route('contacts')}}" class="menu_link">контакты</a></li>
+                    <li class="menu_item"><a href="{{route('contacts')}}" class="menu_link {{active_menu(\Illuminate\Support\Facades\Route::currentRouteName(),'contacts',0,8 )}}">контакты</a></li>
                     <li class="menu_item"><a href="{{route('about')}}" class="menu_link {{active_menu(\Illuminate\Support\Facades\Route::currentRouteName(),'about',0,5 )}} ">о нас</a></li>
                 </ul>
             </div>
